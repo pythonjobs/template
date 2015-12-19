ce37b762ca165488d51cba89db416597379f775c
Steve Stagg <stestagg@gmail.com>
2015-12-19 18:25:35 +0000

    Add in match highlighting and excerpt downloading (incomplete) Update requirements for indexing

--- hyde/content/media/js/search.js
+++ hyde/content/media/js/search.js
@@ -1,205 +1,242 @@
-"use strict";
+/* jshint strict: true, undef: true */
+/* globals jQuery,window */
+(function() {
+  "use strict";  
 
-if (!Array.isArray) Array.isArray = function(arg) {
-	return Object.prototype.toString.call(arg) === '[object Array]';
-}
+  if (!Array.isArray) Array.isArray = function(arg) {
+    return Object.prototype.toString.call(arg) === '[object Array]';
+  };
 
-if (!Object.keys) Object.keys = function(o) {
-	var k=[],p;
-	for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
-	return k;
-}
+  if (!Object.keys) Object.keys = function(o) {
+    var k=[],p;
+    for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+    return k;
+  };
 
-if (!Object.values) Object.values = function(o, f) {
-	var v=[],p;
-	if (f) {
-		for (p in o) if (Object.prototype.hasOwnProperty.call(o,p) && f(o[p])) v.push(o[p]);	
-	} else {
-		for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) v.push(o[p]);
-	}
-	return v;
-}
+  if (!Object.values) Object.values = function(o, f) {
+      var v=[],p;
+      if (f) { 
+        for (p in o) if (Object.prototype.hasOwnProperty.call(o,p) && f(o[p])) v.push(o[p]);
+      } else {
+        for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) v.push(o[p]);
+      }
+      return v;
+  };
 
-jQuery(function ($) {
-	"use strict"
+  function escapeRegExp(str) {
+   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  }
 
-	var job_list = null;
-	var root_index = null;
-	var index_loading = false;
+  jQuery(function ($) {
 
-	var search_box = $("#text_search");
-	var search_info = $("#search_info");
-	var search_id = null;
+    var job_list = null;
+    var root_index = null;
+    var index_loading = false;
 
-	function pend_search() {
-		load_index();
-		if (search_id) { return; }
-		search_id = window.setTimeout(function() { search_id=null; do_search(); }, 100);
-	}
+    var search_box = $("#text_search");
+    var search_info = $("#search_info");
+    var search_id = null;
 
-	function load_index() {
-		if (index_loading) { return; }
-		index_loading = true;
-		$.getJSON("/text_index.json")
-			.then(function(data) {
-				job_list = data[0];
-				root_index = data[1];
-				do_search()
-			})
-			.error(function(e) {
-				console.log(e);
-			})
-	}
+    function load_index() {
+      if (index_loading) { return; }
+      index_loading = true;
+      $.getJSON("/text_index.json")
+        .then(function(data) {
+          job_list = data[0];
+          root_index = data[1];
+          do_search();
+        })
+        .error(function(e) {
+          window.console.log(e);
+        });
+    }
 
-	function stem(term) {
-		return term;
-	}
+    function pend_search() {
+      load_index();
+      if (search_id) { return; }
+      search_id = window.setTimeout(function() { search_id=null; do_search(); }, 100);
+    }
 
-	function find_leaves(node) {
-		if (Array.isArray(node)) {
-			return node;
-		}
-		var found = ("" in node) ? node[''] : [];
-		for (var key in node) {
-			var leaves = find_leaves(node[key]);
-			found = found.concat(leaves);
-		}
-		return found;
-	}
+    function find_leaves(node) {
+      if (Array.isArray(node)) {
+        return node;
+      }
+      var found = ("" in node) ? node[''] : [];
+      for (var key in node) {
+        var leaves = find_leaves(node[key]);
+        found = found.concat(leaves);
+      }
+      return found;
+    }
 
-	function match_substr(lookup, value) {
-		for (var i=1; i<=value.length; ++i){
-			var test = value.slice(0,i);
-			if (test in lookup) {
-				return test;
-			}
-			for (var prefix in lookup) {
-				if (prefix.indexOf(value) == 0) {
-					return prefix;
-				}
-			}
-		}
-	}
+    function match_substr(lookup, value) {
+      for (var i=1; i<=value.length; ++i){
+        var test = value.slice(0,i);
+        if (test in lookup) {
+          return test;
+        }
+        for (var prefix in lookup) {
+          if (prefix.indexOf(value) === 0) {
+            return prefix;
+          }
+        }
+      }
+    }
 
-	function find_matches(val) {
-		var current = root_index;
-		var rest = val;
-		while (rest) {
-			if (Array.isArray(current)) { return []; }
-			var match = match_substr(current, rest);
-			if (!match) { return []; }
-			current = current[match];
-			rest = rest.slice(match.length);
-		}
-		if (current !== root_index) {
-			return find_leaves(current);
-		}
-		return [];
-	}
+    function find_matches(val) {
+      var current = root_index;
+      var rest = val;
+      while (rest) {
+        if (Array.isArray(current)) { return []; }
+        var match = match_substr(current, rest);
+        if (!match) { return []; }
+        current = current[match];
+        rest = rest.slice(match.length);
+      }
+      if (current !== root_index) {
+        return find_leaves(current);
+      }
+      return [];
+    }
 
-	function clear_search(msg) {
-		update_ui();
+    function clear_search(msg) {
+      update_ui();
 
-		search_info.toggleClass("hidden", msg ? false : true);
-		if (msg) {
-			search_info.text(msg);
-		}
-	}
+      search_info.toggleClass("hidden", msg ? false : true);
+      if (msg) {
+        search_info.text(msg);
+      }
+    }
 
-	function update_ui(jobs) {
-		if (jobs) {
-			var job_lookup = {};
-			$.each(jobs, function(index, job) {
-				job_lookup[job.slug] = index;
-			});
+    function update_ui(jobs) {
+      var all_jobs, matched;
 
-			function sort_info(el) {
-				var slug = $(el).data('slug');
-				var order = $(el).data('order');
-				var score = job_lookup[slug].score;
-				score = score > 8 ? 8 : score;
-				return {score: score, order: parseInt(order, 10)};
-			}
+      if (jobs) {
+        var job_lookup = {};
+        $.each(jobs, function(index, job) {
+          job_lookup[job.slug] = index;
+        });
 
-			var all_jobs = $(".job");
-			var matched = all_jobs.filter(function() {
-				return $(this).data('slug') in job_lookup;
-			}).sort(function(el_a, el_b) {
-				var a_info = sort_info(el_a);
-				var b_info = sort_info(el_b);
-				var score_diff = a_info.score - b_info.score;
-				return score_diff || a_info.order - b_info.order;
-			});
-		} else {
-			var all_jobs = matched = $(".job").sort(function(a, b) {
-				return parseInt($(a).data('order'), 10)-parseInt($(b).data('order'), 10);
-			});
-		}
-		if (matched.length == 0) {
-			return clear_search("No matches found, or word too common");
+        var sort_info = function(el) {
+          var slug = $(el).data('slug');
+          var order = $(el).data('order');
+          var score = job_lookup[slug].score;
+          score = score > 8 ? 8 : score;
+          return {score: score, order: parseInt(order, 10)};
+        };
 
-		}
-		all_jobs.hide();
-		matched.show();
+        all_jobs = $(".job");
+        matched = all_jobs.filter(function() {
+          return $(this).data('slug') in job_lookup;
+        }).sort(function(el_a, el_b) {
+          var a_info = sort_info(el_a);
+          var b_info = sort_info(el_b);
+          var score_diff = a_info.score - b_info.score;
+          return score_diff || a_info.order - b_info.order;
+        });
+      } else {
+        all_jobs = matched = $(".job").sort(function(a, b) {
+          return parseInt($(a).data('order'), 10)-parseInt($(b).data('order'), 10);
+        });
+      }
+      if (!matched.length) {
+        return clear_search("No matches found");
 
-		var num_jobs = all_jobs.length;
-		var num_matches = matched.length;
-		search_info.removeClass("hidden");
-		search_info.text("Showing " + num_matches + " of " + num_jobs + " jobs");
-		matched.detach().appendTo($("section.job_list"));
-		
-	}
+      }
+      all_jobs.find('.search_match').text("");
+      var not_matched = all_jobs.not(matched);
+      not_matched.hide();
+      matched.show();
 
-	function do_search() {
-		load_index();
-		if (root_index === null ||	job_list === null) { return; }
-		var search_text = search_box.val().toLowerCase();
-		if (!search_text) { clear_search(); return; }
-		var parts = search_text.split(/\W+/g);
-		
-		var results = {};
+      if (jobs){
+        matched.addClass('needs_excerpt');
+        not_matched.removeClass('needs_excerpt');
+      } else {
+        all_jobs.removeClass('needs_excerpt');
+      }
 
-		function get_result(m) {
-			if (!(m in results)) {
-				results[m] = {
-					slug: job_list[m],
-					count: 0,
-					score: 0
-				}
-			}
-			return results[m];
-		}
+      var num_jobs = all_jobs.length;
+      var num_matches = matched.length;
+      search_info.removeClass("hidden");
+      search_info.text("Showing " + num_matches + " of " + num_jobs + " jobs");
+      matched.detach().appendTo($("section.job_list"));
+      load_an_excerpt();
+    }
 
-		parts.forEach(function(part) {
-			var part_tally = {};
-			var term = stemmer(part)
-			console.log(part, term)
-			var matches = find_matches(term);
-			matches.forEach(function(m) {
-				var match = m[0], value = m[1];
-				var result = get_result(match);
-				if (!(match in part_tally)) {
-					part_tally[match] = true;
-					result.count += 1;
-				}
-				result.score += value;
-			})
-		});
+    search_box.change(function(){
+      pend_search();
+    });
 
-		var results = Object.values(results);
-		results = $.grep(results, function(result) { 
-			return result.count == parts.length; 
-		});
-		update_ui(results);
-		return results;
-	}
+    search_box.keyup(function() {
+      pend_search();
+    });
 
-	search_box.change(function(){
-		pend_search();
-	});
+    function do_search() {
+      load_index();
+      if (root_index === null ||  job_list === null) { return; }
+      var search_text = search_box.val().toLowerCase();
+      if (!search_text) { clear_search(); return; }
+      var parts = search_text.split(/\W+/g);
 
-	search_box.keyup(function() {
-		pend_search();
-	})
+      var results = {};
 
-});
+      function get_result(m) {
+        if (!(m in results)) {
+          results[m] = {
+            slug: job_list[m],
+            count: 0,
+            score: 0
+          };
+        }
+        return results[m];
+      }
+
+      parts.forEach(function(part) {
+        var part_tally = {};
+        var term = window.stemmer(part);
+        var matches = find_matches(term);
+        matches.forEach(function(m) {
+          var match = m[0], value = m[1];
+          var result = get_result(match);
+          if (!(match in part_tally)) {
+            part_tally[match] = true;
+            result.count += 1;
+          }
+          result.score += value;
+        });
+      });
+
+      results = Object.values(results);
+      results = $.grep(results, function(result) {
+        return result.count == parts.length;
+      });
+      update_ui(results);
+      return results;
+    }
+
+    function highlight_matches(data){
+      var parts = search_box.val().split(/\W+/g);
+      var re_matcher = [];
+      parts.forEach(function(part) { re_matcher.push(escapeRegExp(part));});
+      var re = new RegExp("\\b(" + re_matcher.join("|") +")", 'ig');
+      var content = $("<div>").text(data).html();
+      content = content.replace(re, '<span class="match">$&</span>')
+      return content;
+    }
+
+    function load_an_excerpt(el){
+      var the_job = $(".job.needs_excerpt").first();
+      if (!the_job.length) return;
+
+      var slug = the_job.data('slug');
+      $.get("/excerpts/" + slug + ".txt")
+        .then(function(data) {
+          the_job.removeClass('needs_excerpt');
+          var excerpt = highlight_matches(data);
+          $(".search_match", the_job).html(excerpt);
+          load_an_excerpt();
+        });
+    }
+
+  });
+})();
--- hyde/siteindex.py
+++ hyde/siteindex.py
@@ -7,7 +7,7 @@ import bs4
 import stemming.porter2 as porter2
 import re
 
-from fin.contextlog import Log
+from fin.contextlog import Log, CLog
 import fin.cache
 import hyde.plugin
 import jinja2
@@ -28,11 +28,13 @@ class Tree(object):
             nodes.setdefault(key, 0)
             nodes[key] += value
 
-    def collapse(self, start=None):
+    def collapse(self, theshold, start=None):
+        # Note: threshold isn't currently used, but could be used to get rid of terms that are
+        # /too/ common.
         if start is None:
             out = {}
             for prefix, children in self.tree.iteritems():
-                new_prefix, children = self.collapse(children)
+                new_prefix, children = self.collapse(theshold, children)
                 if new_prefix == "":
                     out[prefix] = children
                 else:
@@ -40,7 +42,7 @@ class Tree(object):
             return out
         matches, children = start
         if len(matches) == 0 and len(children) == 1:
-            child_prefix, new_val = self.collapse(children.values()[0])
+            child_prefix, new_val = self.collapse(theshold, children.values()[0])
             return (children.keys()[0] + child_prefix, new_val)
         else:
             sorted_matches = sorted(matches.items(), key=operator.itemgetter(1), reverse=True)
@@ -48,7 +50,7 @@ class Tree(object):
             if sorted_matches:
                 out[''] = sorted_matches
             for child_prefix, value in children.iteritems():
-                new_prefix, new_value = self.collapse(start=value)
+                new_prefix, new_value = self.collapse(theshold, start=value)
                 out[child_prefix + new_prefix] = new_value
             if out.keys() == ['']:
                 out = out['']
@@ -67,7 +69,7 @@ class IndexerPlugin(hyde.plugin.Plugin):
     def add_to_index(self, resource, texts):
         resource_index = len(self.resources)
         self.resources.append(resource.slug)
-        for text, weight in texts:
+        for _, text, weight in texts:
             words = re.split(r"\W+", text, flags=re.U)
             for word in words:
                 if word == "" or word.isdigit():
@@ -78,25 +80,48 @@ class IndexerPlugin(hyde.plugin.Plugin):
                 self.by_word[stemmed][resource_index] += weight
 
     def text_resource_complete(self, resource, text):
-        if not resource.source_file.kind == 'html':
-            return
-        if not resource.source_file.is_descendant_of(self.jobs_dir):
-            return
+        with CLog("Indexing %s" % resource):
+            if not resource.source_file.kind == 'html':
+                return
+            if not resource.source_file.is_descendant_of(self.jobs_dir):
+                return
 
-        doc = bs4.BeautifulSoup(text, "html.parser")
-        body = doc.find(attrs={"class": 'body'})
-        headings = body.select("h1,h2")
-        for heading in headings:
-            heading.extract()
-        tags = " ".join(resource.meta.get('tags', ''))
-        company = resource.meta.get('company', '')
-        parts = [
-            (body.text, 1),
-            (tags, 5),
-            (company, 5)
-        ]
-        self.write_text_resource(resource.slug, company, tags, body)
-        self.add_to_index(resource, parts)
+            doc = bs4.BeautifulSoup(text, "html.parser")
+            body = doc.find(attrs={"class": 'body'})
+            headings = body.select("h1,h2")
+            for heading in headings:
+                heading.extract()
+            tags = " ".join(resource.meta.get('tags', ''))
+            company = resource.meta.get('company', '')
+            parts = [
+                ("Company: ",company, 5),
+                ("Tags: ", tags, 5),
+                ("", body.text, 1),
+            ]
+            self.write_text_resource(resource.slug, parts)
+            self.add_to_index(resource, parts)
+
+    def write_text_resource(self, name, parts):
+        text_content = []
+        for prefix, content, _ in parts:
+            collapsed_content = " ".join(content.splitlines()).strip()
+            text_content.append("%s%s" % (prefix, collapsed_content))
+        text_content = ".\n".join(text_content)
+        self.deploy(text_content, "excerpts/%s.txt" % (name, ))
+
+    def deploy(self, content, rel_path):
+        with CLog("Deploying file: %s" % rel_path):
+            with CLog("Updating resource"):
+                target = self.site.config.content_root_path.child_file(rel_path)
+                res = self.site.content.add_resource(target)
+                target.parent.make()
+                with open(target.path, "wb") as fh:
+                    fh.write(content.encode("utf-8"))
+            with CLog("Copying to dest"):
+                # Because this is called from node_complete, it's too late to let the deployer
+                # copy the file for us, so have to do it manually
+                target = File(self.site.config.deploy_root_path.child(res.relative_deploy_path))
+                res.source_file.copy_to(target)
 
     def node_complete(self, node):
         if node.path != self.jobs_dir:
@@ -106,22 +131,11 @@ class IndexerPlugin(hyde.plugin.Plugin):
             threshold = num_resource * self.INDEX_FREQUENCY_LIMIT
             tree = Tree()
             for word, by_resource in self.by_word.iteritems():
-                if len(by_resource) > threshold:
-                    continue
                 tree.add(word, by_resource.items())
-            self.write_index((self.resources, tree.collapse()))
+            self.write_index((self.resources, tree.collapse(threshold)))
 
     def write_index(self, index):
         with Log("Encoding as JSON") as l:
             data = json.dumps(index, separators=",:")
             l.output("%s bytes" % len(data))
-        with Log("Updating resource"):
-            target = self.site.config.content_root_path.child_file("text_index.json")
-            res = self.site.content.add_resource(target)
-            with open(target.path, "wb") as fh:
-                fh.write(data)
-        with Log("Copying to dest"):
-            # Because this is called from node_complete, it's too late to let the deployer
-            # copy the file for us, so have to do it manually
-            target = File(self.site.config.deploy_root_path.child(res.relative_deploy_path))
-            res.source_file.copy_to(target)
+        self.deploy(data, "text_index.json")
