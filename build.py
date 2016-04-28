05da52c520362ddf4e653f94fcd3ddc6ccc18b64
Daniel Pope <mauve@mauveweb.co.uk>
2016-04-28 17:37:49 +0100

    Pre-validate things that will crash the parser; also misnamed files

--- build.py
+++ build.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python2.7
+import re
 import sys
 import shutil
 import os
+import os.path
 import subprocess
+import yaml
 from fin.contextlog import Log
+
+
+def validate(fullpath):
+    """Validate the formatting of the given jobs file.
+
+    The contents of the file will be validated later; this is just checking
+    that the file will parse.
+
+    """
+    data = open(fullpath, 'r').read()
+    path = os.path.basename(fullpath)
+
+    try:
+        data = data.decode('utf8')
+    except UnicodeDecodeError:
+        raise AssertionError("%s: is not valid UTF-8" % path)
+    chunks = re.split(r'^---[ \t]*$', data, flags=re.M)
+    assert len(chunks) >= 3, \
+        "%s: should be YAML and Markdown, prefixed by --- lines" % path
+    assert not chunks[0].strip(), \
+        "%s: data before initial ---" % path
+
+    try:
+        yaml.safe_load(chunks[1])
+    except Exception as e:
+        raise AssertionError("%s: malformed YAML: %s" % (path, e))
 
 
 def main(jobs_dir):
@@ -18,13 +47,14 @@ def main(jobs_dir):
 
     with Log("Copy in jobs") as l:
         for file in os.listdir(jobs_source):
-            if not file.endswith(".html"):
-                l.output("Skipping: %s" % file)
-                continue
+            assert file.endswith(".html"), \
+                "%s: jobs files must end in .html" % file
             with Log('Copying %s' % file):
                 src_path = '%s/%s' % (jobs_source, file) # This is safer than join()
                 dest_path = '%s/%s' % (jobs_dest, file)
+                validate(src_path)
                 shutil.copyfile(src_path, dest_path)
+
     with Log("Building Site"):
         subprocess.check_call(['hyde', '-x', '-s', hyde_root, 'gen', '-r'])
 
