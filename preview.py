1733862a4adfc1785d6befe0ddaadf10a6506569
Stephen Stagg <stestagg@gmail.com>
2018-04-26 00:38:49 +0100

    More preview fixes

--- preview.py
+++ preview.py
@@ -4,6 +4,9 @@ import time
 import sys
 import subprocess
 import requests
+import urllib
+
+from fin.contextlog import Log
 
 from selenium import webdriver
 
@@ -19,51 +22,56 @@ def upload_screenshot(data):
 
 def get_preview(driver, filename, pull_request_num):
     basename, ext = os.path.splitext(filename)
-    url = "http://localhost:8080/jobs/%s.html" % basename
-    driver.get(url)
-    while driver.execute_script("return document.readyState") != "complete":
-        time.sleep(0.5)
-    png = driver.get_screenshot_as_png()
-    return upload_screenshot(png)
+    url = "http://localhost:8080/jobs/%s.html" % urllib.quote(basename)
+    with Log(url):
+        driver.get(url)
+        while driver.execute_script("return document.readyState") != "complete":
+            time.sleep(0.5)
+        png = driver.get_screenshot_as_png()
+    with Log("Uploading Screenshot"):
+        return upload_screenshot(png)
 
 
 def get_file_previews(pull_request_num):
-    preview_ids = []
-    driver = webdriver.PhantomJS()
-    hyde_root = os.path.join(TEMPLATE_DIR, 'hyde')
-    server_proc = subprocess.Popen(['hyde', '-s', hyde_root, 'serve'])
-    time.sleep(4)
-    try:
-        driver.set_window_size(800, 600)
-        for filename in get_modified_files():
-            image_id = get_preview(driver, filename, pull_request_num)
-            preview_ids.append(image_id)
-    finally:
-        driver.close()
-        server_proc.terminate()
-        server_proc.wait()
+    with Log("Generating Previews"):
+        preview_ids = []
+        driver = webdriver.PhantomJS()
+        hyde_root = os.path.join(TEMPLATE_DIR, 'hyde')
+        with Log("Starting Server"):
+            server_proc = subprocess.Popen(['hyde', '-s', hyde_root, 'serve'])
+            time.sleep(4)
+        try:
+            driver.set_window_size(800, 600)
+            for filename in get_modified_files():
+                with Log(filename):
+                    image_id = get_preview(driver, filename, pull_request_num)
+                    preview_ids.append(image_id)
+        finally:
+            driver.close()
+            server_proc.terminate()
+            server_proc.wait()
 
-    if not preview_ids:
-        return
+        if not preview_ids:
+            return
 
-    link_template = '![Job listing preview](https://s3-eu-west-1.amazonaws.com/pythonjobs-screenshots/%s.png)'
-    image_links = [link_template % preview_id for preview_id in preview_ids]
+        link_template = '![Job listing preview](https://s3-eu-west-1.amazonaws.com/pythonjobs-screenshots/%s.png)'
+        image_links = [link_template % preview_id for preview_id in preview_ids]
 
-    message = """Here are some screenshots of what the live listing should look like:
+        message = """Here are some screenshots of what the live listing should look like:
 
 %s
-    """ % ("\n".join(image_links), )
+        """ % ("\n".join(image_links), )
 
-    proc = subprocess.Popen([
-        sys.executable,
-        os.path.join(TEMPLATE_DIR, "comment.py")
-    ], stdin=subprocess.PIPE)
-    proc.communicate(message)
+        proc = subprocess.Popen([
+            sys.executable,
+            os.path.join(TEMPLATE_DIR, "comment.py")
+        ], stdin=subprocess.PIPE)
+        proc.communicate(message)
 
 
 def get_modified_files():
     modified_files = subprocess.check_output(['git', 'diff', '--name-only', 'master'])
-    for line in modified_files.split():
+    for line in modified_files.splitlines():
         if line.startswith("jobs/"):
             yield line[5:]
 
